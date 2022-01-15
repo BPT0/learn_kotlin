dd022d2e03822b02e8b811534265a52ce5f97511
PTFDB <kbs95123@gmail.com>
2022-01-15 12:55:45 +0900

    과제1풀이

--- kotlin_base/kotlin/07_과제/과제1.kts
+++ kotlin_base/kotlin/07_과제/과제1.kts
@@ -1,8 +1,8 @@
 // 1번
-var num1 : Int = 10
+/*var num1 : Int = 10
 var num2 : Int = 20
 var iseQual = if(num1 == num2) true else false
-println(iseQual)
+println(iseQual)*/
 // 자료형이 다르면 비교연산자를 사용할수 없다
 /*if(num1 == num2){
     var isequal : Boolean = true
@@ -42,13 +42,25 @@ fun gradeTest2(correectAnswer: Int):Int = correectAnswer *5
 println(gradeTest(10))
 println(gradeTest2(10))*/
 
+// 5번
 fun plusNum(firstNum:Int?, secondNum:Int?):Int{
-    if(firstNum == null){
+    // 함수에서 받은 인자 firstNum, secondNum -> val이다
+    /*if(firstNum == null){
         return firstNum =0
     }
     if(secondNum == null){
         return secondNum = 0
     }
-    return firstNum+secondNum
+    return firstNum+secondNum*/
+    // 정답1
+    val first : Int = if(firstNum == null)0 else firstNum
+    val second : Int = if(secondNum==null)0 else secondNum
+    return first+second
+    // 오답1. 파라미터로 사용한 변수명을 함수내에서 중복으로 같은 변수명을 선언할 수 없다
+    // 함수안에서 똑같이 이름을 갖고 있는 다르 변수가 있으면 안된다
+    /*val firstNum: Int? = if(firstNum == null)0 else firstNum
+    val secondNum : Int? = if(secondNum==null)0 else secondNum
+    return first+second*/
 }
+println(plusNum(null, null))
 
